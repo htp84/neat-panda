84974f0c9cfb2c06dcb740d56fe86a3fb427eb06
Henric Sundberg <henric.sundberg@gmail.com>
2019-03-21 19:16:09 +0100

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/htp84/neat_panda",
-    version="0.6.1",
+    version="version='0.6.2'",
     zip_safe=False,
 )
