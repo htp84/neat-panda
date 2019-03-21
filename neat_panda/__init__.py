ac4c0e6fac23f247b2765dcfa2daa255164906ae
Henric Sundberg <henric.sundberg@gmail.com>
2019-03-21 19:01:19 +0100

    Bump version: 0.5.0 → 0.6.0

--- neat_panda/__init__.py
+++ neat_panda/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Henric Sundberg"""
 __email__ = "henric.sundberg@gmail.com"
-__version__ = "__version__ = '0.5.0'"
+__version__ = "__version__ = '0.6.0'"
 
 from .neat_panda import spread, gather
